91c043ae42c823f2fcd5bbc0e40ecb5e43ae2ca7
Hoakin26 <joaquingsuy93@gmail.com>
2026-08-21 10:09:12 -0600

    Subiendo Archivo Visitas Medicas

---
+++ visitas-medicas/src/main/java/microservicios/visitas_medicas/VisitasMedicasApplication.java
@@ -0,0 +1,13 @@
+package microservicios.visitas_medicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitasMedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitasMedicasApplication.class, args);
+	}
+
+}
---
+++ visitas-medicas/src/test/java/microservicios/visitas_medicas/VisitasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package microservicios.visitas_medicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitasMedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
